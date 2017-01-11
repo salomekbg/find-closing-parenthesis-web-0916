6082eb0a465def2d74eb301cac87ba3cd42b43c4
SluvsP <salomekbg@gmail.com>
2017-01-11 17:20:41 -0500

    completed and passing test

--- find_closing_parenthesis.rb
+++ find_closing_parenthesis.rb
@@ -1,3 +1,16 @@
 def find_closing_parenthesis(sentence, open_position)
+  array = sentence.split('')
+  next_position = open_position + 1
+  count = 0
 
+  while next_position < array.length
+    if array[next_position] == ")" && count == 0
+      return next_position
+    elsif array[next_position] == ")" && count != 0
+      count -= 1
+    else
+      count += 1
+    end
+    next_position += 1
+  end
 end
